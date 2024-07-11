1cc585be723d7af9ff90535858e6ee23fc8b5ce2
Franco <francotorrico1999@gmail.com>
2024-07-11 13:46:56 -0300

    first commit

--- Backend/src/main/java/com/medical/app/repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medical.app.repository;
-
-import com.medical.app.models.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
