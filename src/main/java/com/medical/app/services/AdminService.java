694e4acdfbbb8fecd58a9499fb19122cfa5ed5a5
Francotorrico <95579148+Francotorrico@users.noreply.github.com>
2024-07-11 20:59:53 -0300

    Delete src directory

--- src/main/java/com/medical/app/services/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.medical.app.services;
-
-import com.medical.app.models.Admin;
-
-public interface AdminService {
-
-    Admin findAdmin(Admin admin);
-    Admin saveAdmin(Admin admin);
-}
